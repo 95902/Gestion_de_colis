39d097d6ceafdaec91515401cbe20f93ff4753ac
MarcelBragance <bragance.m@hotmail.fr>
2020-12-15 10:42:38 +0100

    Modèle && Controlleur Commande

--- Gestion_de_colis/src/controleur/Controleur.java
+++ Gestion_de_colis/src/controleur/Controleur.java
@@ -4,7 +4,9 @@ import modele.Modele;
 import java.util.ArrayList;
 public class Controleur 
 {
-
+    /***************************
+    ********USER**************
+    **************************/ 
     public static void insertUser (User unUser) 
 	{
 		//ici : on peut réaliser des contrôles sur les données de ce client avant son insertion en BDD
@@ -30,4 +32,34 @@ public class Controleur
 		
 		Modele. updateUser( unUser);
 	}
+
+    /*****************************
+    *******COMMANDE***************
+    *****************************/
+
+    public static void insertCmd (Commande uneCommande) 
+	{
+		//ici : on peut réaliser des contrôles sur les données de ce client avant son insertion en BDD
+		
+	    Modele.insertCmd(uneCommande); 
+	}
+	
+	public static  ArrayList<Commande> selectAllCommande()
+	{
+		return Modele.selectAllCommande();
+	}
+	
+	public static void deleteCommande(int idcommande)
+	{
+		//ici : on peut réaliser des contrôles sur les données de ce client avant son insertion en BDD
+		
+		Modele.deleteCommande(idcommande);
+	}
+	
+	public static void  updateCommande (Commande uneCommande)
+	{
+		//ici : on peut réaliser des contrôles sur les données de ce client avant son insertion en BDD
+		
+		Modele. updateCommande( uneCommande);
+	} 
 }
--- Gestion_de_colis/src/modele/Modele.java
+++ Gestion_de_colis/src/modele/Modele.java
@@ -42,7 +42,7 @@ public class Modele {
 
 		ArrayList<User> lesUser = new  ArrayList<User>() ;
 
-		String requete ="slect * from _user;";
+		String requete ="select * from _user;";
 
 		try
 		{
@@ -98,10 +98,10 @@ public class Modele {
 
 	public static void updateUser (User unUser)
 	{
-		String requete = "update _user set nom='"+ unUser.getNom()
-			+"','"+ unUser.getPrenom() +"','"+unUser.getAdresse()
-			+"','"+ unUser.getType()+"','"+unUser.getTelephone()
-			+"','"+ unUser.getMdp() +"','"+unUser.getVille()+"','"+unUser.getCode_postale()
+		String requete = "update _user set nom_user='"+ unUser.getNom()
+			+"',prenom_user='"+ unUser.getPrenom() +"',adresse_user='"+unUser.getAdresse()
+			+"',type_user='"+ unUser.getType()+"',telephone_user='"+unUser.getTelephone()
+			+"',mdp_user'"+ unUser.getMdp() +"',ville_user'"+unUser.getVille()+"',code_postal_user'"+unUser.getCode_postale()
 			+"' where id_user ="+ unUser. getId_user()
 			+"');";
 		
@@ -149,7 +149,85 @@ public class Modele {
 			}
 		}
 	}
+
+	public static ArrayList<Commande> selectAllCommande(){
+
+		ArrayList<Commande> lesCommandes = new  ArrayList<Commande>() ;
+
+		String requete ="select * from commande;";
+
+		try
+		{
+			uneBDD.seConnecter();
+			Statement unStat = uneBDD.getMaConnexion().createStatement();
+			ResultSet desResultats = unStat.executeQuery(requete);
+
+			while (desResultats.next())
+			{
+				Commande uneCommande = new Commande
+					(
+						desResultats.getInt("idcommande"),
+						desResultats.getInt("nbcolis"),
+						desResultats.getString("etats"),
+						desResultats.getString("libelle_etats"),
+						desResultats.getString("description_envoie"),
+						desResultats.getString("contenue_envoie"),
+						desResultats.getFloat("montant"),
+						desResultats.getDate("date")
+
+					);
+				lesCommandes.add(uneCommande);
+			}
+			unStat.close();
+			uneBDD.seDeconnecter();
+		}
+		catch(SQLException exp)
+		{
+			System.out.println("Erreur d'execution de la requete : "+requete);
+		}
+		return lesCommandes;
+
+	}
+
+	public static void deleteCommande(int idcommande)
+	{
+		String requete ="delete from client where id_commande="+idcommande+";";
+
+		try
+		{
+			uneBDD.seConnecter();
+			Statement unStat = uneBDD.getMaConnexion().createStatement();
+			unStat.execute(requete);
+			unStat.close();
+			uneBDD.seDeconnecter();
+		}
+		catch(SQLException exp) 
+		{
+			System.out.println("Erreur d'execution de la requete : "+requete);
+		}
+	}
 	
+	public static void updateCommande (Commande uneCommande)
+	{
+		String requete = "update commande set nb_colis='"+ uneCommande.getNbcolis()
+			+"',etats_envoi='"+ uneCommande.getEtats() +"',date_commande='"+uneCommande.getDate()
+			+"',libelle_etats_envoi='"+ uneCommande.getLibelle_etats()+"',description_envoie='"+uneCommande.getDescription_envoie()
+			+"',contenue_envoie='"+ uneCommande.getContenue_envoie() +"',montant_commande='"+uneCommande.getMontant()
+			+"');";
+		
+		try
+		{
+			uneBDD.seConnecter();
+			Statement unStat = uneBDD.getMaConnexion().createStatement();
+			unStat.execute(requete);
+			unStat.close();
+			uneBDD.seDeconnecter();
+		}
+		catch(SQLException exp) 
+		{
+			System.out.println("Erreur d'execution de la requete : "+requete);
+		}
+	}
 }
 
 
